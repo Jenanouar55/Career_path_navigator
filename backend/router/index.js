cf57b0727527baaff97fe72b24c4d6b21d515fb7
Mary-Maina <richellwanjiru@gmail.com>
2025-01-04 09:08:03 +0300

    Created the app controller

--- backend/router/index.js
+++ backend/router/index.js
@@ -8,4 +8,9 @@ router.get('/:userId', UserController.getUser);
 router.put('/:userId', UserController.updateUser);
 router.delete('/:userId', UserController.deleteUser);
 
+// App Routes
+router.get('/app/status', AppController.getStatus);  // For checking app status (Redis and DB)
+router.get('/app/stats', AppController.getStats);
+
+
 export default router;
