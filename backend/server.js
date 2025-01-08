2585a6f107b82b4cb58cb7bbf39cc51872d10c0b
Mary-Maina <richellwanjiru@gmail.com>
2025-01-08 05:10:50 +0300

    updated the server.js to include the router

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
+import router from './router/index';
 
 const app = express();
 
